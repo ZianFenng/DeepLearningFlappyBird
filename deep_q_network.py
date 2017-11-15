ac44b53b87087545b47cbc8886c866b9e152ccd9
ajklili <ustcandroid@gmail.com>
2017-11-15 00:41:21 -0500

    add comments and tf summary and rename something

--- deep_q_network.py
+++ deep_q_network.py
@@ -5,37 +5,43 @@ import tensorflow as tf
 import cv2
 import sys
 sys.path.append("game/")
-import wrapped_flappy_bird as game
+import wrapped_flappy_bird as flappy_bird
 import random
 import numpy as np
 from collections import deque
 
-GAME = 'bird' # the name of the game being played for log files
-ACTIONS = 2 # number of valid actions
-GAMMA = 0.99 # decay rate of past observations
-OBSERVE = 100000. # timesteps to observe before training
-EXPLORE = 2000000. # frames over which to anneal epsilon
-FINAL_EPSILON = 0.0001 # final value of epsilon
-INITIAL_EPSILON = 0.0001 # starting value of epsilon
-REPLAY_MEMORY = 50000 # number of previous transitions to remember
-BATCH = 32 # size of minibatch
+GAME = 'bird'  # the name of the game being played for log files
+ACTIONS = 2  # number of valid actions
+GAMMA = 0.99  # decay rate of past observations
+OBSERVE = 100000.  # timesteps to observe before training
+EXPLORE = 2000000.  # frames over which to anneal epsilon
+FINAL_EPSILON = 0.0001  # final value of epsilon
+INITIAL_EPSILON = 0.0001  # starting value of epsilon
+REPLAY_MEMORY = 50000  # number of previous transitions to remember
+BATCH = 32  # size of minibatch
 FRAME_PER_ACTION = 1
 
+
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev = 0.01)
+    initial = tf.truncated_normal(shape, stddev=0.01)
     return tf.Variable(initial)
+
 
 def bias_variable(shape):
-    initial = tf.constant(0.01, shape = shape)
+    initial = tf.constant(0.01, shape=shape)
     return tf.Variable(initial)
 
+
 def conv2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
+    return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding="SAME")
+
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+
 
 def createNetwork():
+    """ create a CNN """
     # network weights
     W_conv1 = weight_variable([8, 8, 4, 32])
     b_conv1 = bias_variable([32])
@@ -53,137 +59,133 @@ def createNetwork():
     b_fc2 = bias_variable([ACTIONS])
 
     # input layer
-    s = tf.placeholder("float", [None, 80, 80, 4])
+    input = tf.placeholder("float", [None, 80, 80, 4], name="input")
 
     # hidden layers
-    h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
+    h_conv1 = tf.nn.relu(conv2d(input, W_conv1, 4) + b_conv1, name="h_conv1")
     h_pool1 = max_pool_2x2(h_conv1)
 
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
-    #h_pool2 = max_pool_2x2(h_conv2)
-
-    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
-    #h_pool3 = max_pool_2x2(h_conv3)
-
-    #h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
-    h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2, name="h_conv2")
+    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3, name="h_conv3")
+    h_conv3_flat = tf.reshape(h_conv3, [-1, 1600], name="h_conv3_flat")
+    # h_pool2 = max_pool_2x2(h_conv2)
+    # h_pool3 = max_pool_2x2(h_conv3)
+    # h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
 
     h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
 
     # readout layer
     readout = tf.matmul(h_fc1, W_fc2) + b_fc2
 
-    return s, readout, h_fc1
+    return input, readout, h_fc1
 
-def trainNetwork(s, readout, h_fc1, sess):
-    # define the cost function
-    a = tf.placeholder("float", [None, ACTIONS])
-    y = tf.placeholder("float", [None])
-    readout_action = tf.reduce_sum(tf.multiply(readout, a), reduction_indices=1)
-    cost = tf.reduce_mean(tf.square(y - readout_action))
-    train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
 
-    # open up a game state to communicate with emulator
-    game_state = game.GameState()
+def trainNetwork(input, readout, h_fc1, sess):
+    """ define the cost function """
+    a = tf.placeholder("float", [None, ACTIONS], name="a")
+    y = tf.placeholder("float", [None], name="y")
+    readout_action = tf.reduce_sum(tf.multiply(readout, a, name="readout_mul_a"), reduction_indices=1, name="readout_action")
+    cost_mse = tf.reduce_mean(tf.square(y - readout_action), name="cost_mse")  # Lecture7 P33, MSE between Q-network and Q-learning
+    tf.summary.scalar("cost", cost_mse)
+    train_step = tf.train.AdamOptimizer(1e-6).minimize(cost_mse)
 
-    # store the previous observations in replay memory
-    D = deque()
-
+    """ prepare the game """
+    game = flappy_bird.GameState()  # open up a game state to communicate with emulator
+    observations = deque()  # store the previous observations in replay memory
     # printing
     a_file = open("logs_" + GAME + "/readout.txt", 'w')
     h_file = open("logs_" + GAME + "/hidden.txt", 'w')
+    summary_merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter("./logs_tf", sess.graph)
 
-    # get the first state by doing nothing and preprocess the image to 80x80x4
+    """ get the first state by doing nothing and pre-process the image to 80x80x4 """
     do_nothing = np.zeros(ACTIONS)
-    do_nothing[0] = 1
-    x_t, r_0, terminal = game_state.frame_step(do_nothing)
-    x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
-    ret, x_t = cv2.threshold(x_t,1,255,cv2.THRESH_BINARY)
-    s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
-
-    # saving and loading networks
-    saver = tf.train.Saver()
-    sess.run(tf.initialize_all_variables())
-    checkpoint = tf.train.get_checkpoint_state("saved_networks")
+    do_nothing[0] = 1  # [1, 0] not fly, [0, 1] fly
+    x_t, r_0, terminal = game.frame_step(do_nothing)  # x_t is the current image
+    x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)  # to gray image
+    ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)  # to binary image
+    s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)  # duplicate to (80, 80, 4) for input layer: (x_t, x_t, x_t, x_t)
+    
+    """ saving and loading networks """
+    saver = tf.train.Saver()  # trained model from/to file
+    sess.run(tf.global_variables_initializer())  # initialize all variables
+    checkpoint = tf.train.get_checkpoint_state("saved_networks")  # pre-trained model in folder "./saved_networks"
     if checkpoint and checkpoint.model_checkpoint_path:
-        saver.restore(sess, checkpoint.model_checkpoint_path)
+        saver.restore(sess, checkpoint.model_checkpoint_path)  # load trained model from file
         print("Successfully loaded:", checkpoint.model_checkpoint_path)
     else:
         print("Could not find old network weights")
 
-    # start training
+    """ start training """
     epsilon = INITIAL_EPSILON
     t = 0
-    while "flappy bird" != "angry bird":
+    while True:
+        # input s_t into the network and get the values from the network
+        readout_t = readout.eval(feed_dict={input: [s_t]})[0]
         # choose an action epsilon greedily
-        readout_t = readout.eval(feed_dict={s : [s_t]})[0]
-        a_t = np.zeros([ACTIONS])
-        action_index = 0
-        if t % FRAME_PER_ACTION == 0:
-            if random.random() <= epsilon:
-                print("----------Random Action----------")
-                action_index = random.randrange(ACTIONS)
-                a_t[random.randrange(ACTIONS)] = 1
-            else:
-                action_index = np.argmax(readout_t)
-                a_t[action_index] = 1
+        a_t = np.zeros([ACTIONS])  # action (not, fly)
+        action_index = 0  # 0 for not, 1 for fly
+        if t % FRAME_PER_ACTION == 0:  # when player can do action
+            if random.random() <= epsilon:  # not greedy at a small probability
+                action_index = random.randrange(ACTIONS)  # random 0 or 1
+                a_t[random.randrange(ACTIONS)] = 1  # set action
+            else:  # greedy
+                action_index = np.argmax(readout_t)  # index of the max value in readout_t
+                a_t[action_index] = 1  # set action
         else:
-            a_t[0] = 1 # do nothing
+            a_t[0] = 1  # do nothing  # when player cannot do action
 
-        # scale down epsilon
+        # scale down epsilon gradually in EXPLORE steps
         if epsilon > FINAL_EPSILON and t > OBSERVE:
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
         # run the selected action and observe next state and reward
-        x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
+        x_t1_colored, r_t, terminal = game.frame_step(a_t)
         x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
         ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
         x_t1 = np.reshape(x_t1, (80, 80, 1))
-        #s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
-        s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
+        s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)  # (80, 80, 4), (x_t1, x_t, x_t, x_t), why?
 
-        # store the transition in D
-        D.append((s_t, a_t, r_t, s_t1, terminal))
-        if len(D) > REPLAY_MEMORY:
-            D.popleft()
+        # store the transition in observations
+        observations.append((s_t, a_t, r_t, s_t1, terminal))
+        if len(observations) > REPLAY_MEMORY:
+            observations.popleft()
 
-        # only train if done observing
+        # only train if done observing when there is enough episodes
         if t > OBSERVE:
             # sample a minibatch to train on
-            minibatch = random.sample(D, BATCH)
+            minibatch = random.sample(observations, BATCH)
 
             # get the batch variables
-            s_j_batch = [d[0] for d in minibatch]
+            s_t_batch = [d[0] for d in minibatch]
             a_batch = [d[1] for d in minibatch]
             r_batch = [d[2] for d in minibatch]
-            s_j1_batch = [d[3] for d in minibatch]
+            s_t1_batch = [d[3] for d in minibatch]
+            terminal_batch = [d[4] for d in minibatch]
 
             y_batch = []
-            readout_j1_batch = readout.eval(feed_dict = {s : s_j1_batch})
+            readout_t1_batch = readout.eval(feed_dict={input: s_t1_batch})
             for i in range(0, len(minibatch)):
-                terminal = minibatch[i][4]
                 # if terminal, only equals reward
-                if terminal:
+                if terminal_batch[i]:
                     y_batch.append(r_batch[i])
                 else:
-                    y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
+                    y_batch.append(r_batch[i] + GAMMA * np.max(readout_t1_batch[i]))  # Lecture7 P33
 
             # perform gradient step
-            train_step.run(feed_dict = {
-                y : y_batch,
-                a : a_batch,
-                s : s_j_batch}
-            )
+            summary, _ = sess.run([summary_merged, train_step], feed_dict={y: y_batch, a: a_batch, input: s_t_batch})
+            writer.add_summary(summary, t)
 
         # update the old values
         s_t = s_t1
-        t += 1
+        t += 1  # add step
 
+        """ save and print data """
         # save progress every 10000 iterations
         if t % 10000 == 0:
-            saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step = t)
+            saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step=t)
 
-        # print info
+        # print info every step
         state = ""
         if t <= OBSERVE:
             state = "observe"
@@ -191,25 +193,21 @@ def trainNetwork(s, readout, h_fc1, sess):
             state = "explore"
         else:
             state = "train"
+        print("TIMESTEP:", t, ", STATE", state, ", EPSILON:", epsilon, ", ACTION:", action_index, ", REWARD:", r_t, ", Q_MAX: %e" % np.max(readout_t))
 
-        print("TIMESTEP", t, "/ STATE", state, \
-            "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t, \
-            "/ Q_MAX %e" % np.max(readout_t))
         # write info to files
-        '''
         if t % 10000 <= 100:
             a_file.write(",".join([str(x) for x in readout_t]) + '\n')
-            h_file.write(",".join([str(x) for x in h_fc1.eval(feed_dict={s:[s_t]})[0]]) + '\n')
+            h_file.write(",".join([str(x) for x in h_fc1.eval(feed_dict={input: [s_t]})[0]]) + '\n')
             cv2.imwrite("logs_tetris/frame" + str(t) + ".png", x_t1)
-        '''
+    writer.close()
 
-def playGame():
-    sess = tf.InteractiveSession()
-    s, readout, h_fc1 = createNetwork()
-    trainNetwork(s, readout, h_fc1, sess)
 
 def main():
-    playGame()
+    sess = tf.InteractiveSession()  # new tensorflow session
+    input, readout, h_fc1 = createNetwork()
+    trainNetwork(input, readout, h_fc1, sess)
+
 
 if __name__ == "__main__":
     main()
--- game/wrapped_flappy_bird.py
+++ game/wrapped_flappy_bird.py
@@ -7,38 +7,42 @@ import pygame.surfarray as surfarray
 from pygame.locals import *
 from itertools import cycle
 
+""" use pygame to have a game window """
 FPS = 30
-SCREENWIDTH  = 288
+SCREENWIDTH = 288
 SCREENHEIGHT = 512
-
 pygame.init()
 FPSCLOCK = pygame.time.Clock()
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption('Flappy Bird')
 
-IMAGES, SOUNDS, HITMASKS = flappy_bird_utils.load()
-PIPEGAPSIZE = 100 # gap between upper and lower part of pipe
-BASEY = SCREENHEIGHT * 0.79
 
-PLAYER_WIDTH = IMAGES['player'][0].get_width()
-PLAYER_HEIGHT = IMAGES['player'][0].get_height()
-PIPE_WIDTH = IMAGES['pipe'][0].get_width()
-PIPE_HEIGHT = IMAGES['pipe'][0].get_height()
+IMAGES, SOUNDS, HITMASKS = flappy_bird_utils.load()
+PIPEGAPSIZE = 100  # gap between upper and lower part of pipe
+BASEY = SCREENHEIGHT * 0.79  # remain 107.5, base: 336(>288) * 112(>107.5)
+
+PLAYER_WIDTH = IMAGES['player'][0].get_width()  # 34
+PLAYER_HEIGHT = IMAGES['player'][0].get_height()  # 34
+PIPE_WIDTH = IMAGES['pipe'][0].get_width()  # 52
+PIPE_HEIGHT = IMAGES['pipe'][0].get_height()  # 320
 BACKGROUND_WIDTH = IMAGES['background'].get_width()
 
-PLAYER_INDEX_GEN = cycle([0, 1, 2, 1])
+PLAYER_INDEX_GEN = cycle([0, 1, 2, 1])  # ???
 
 
 class GameState:
+
     def __init__(self):
-        self.score = self.playerIndex = self.loopIter = 0
-        self.playerx = int(SCREENWIDTH * 0.2)
-        self.playery = int((SCREENHEIGHT - PLAYER_HEIGHT) / 2)
-        self.basex = 0
+        self.score = 0
+        self.playerIndex = 0
+        self.loopIter = 0
+        self.playerx = int(SCREENWIDTH * 0.2)  # near leftmost
+        self.playery = int((SCREENHEIGHT - PLAYER_HEIGHT) / 2)  # in the middle
+        self.basex = 0  # ???
         self.baseShift = IMAGES['base'].get_width() - BACKGROUND_WIDTH
 
-        newPipe1 = getRandomPipe()
-        newPipe2 = getRandomPipe()
+        newPipe1 = getRandomPipePair()
+        newPipe2 = getRandomPipePair()
         self.upperPipes = [
             {'x': SCREENWIDTH, 'y': newPipe1[0]['y']},
             {'x': SCREENWIDTH + (SCREENWIDTH / 2), 'y': newPipe2[0]['y']},
@@ -50,12 +54,12 @@ class GameState:
 
         # player velocity, max velocity, downward accleration, accleration on flap
         self.pipeVelX = -4
-        self.playerVelY    =  0    # player's velocity along Y, default same as playerFlapped
-        self.playerMaxVelY =  10   # max vel along Y, max descend speed
-        self.playerMinVelY =  -8   # min vel along Y, max ascend speed
-        self.playerAccY    =   1   # players downward accleration
-        self.playerFlapAcc =  -9   # players speed on flapping
-        self.playerFlapped = False # True when player flaps
+        self.playerVelY = 0    # player's velocity along Y, default same as playerFlapped
+        self.playerMaxVelY = 10   # max vel along Y, max descend speed
+        self.playerMinVelY = -8   # min vel along Y, max ascend speed
+        self.playerAccY = 1   # players downward accleration
+        self.playerFlapAcc = -9   # players speed on flapping
+        self.playerFlapped = False  # True when player flaps
 
     def frame_step(self, input_actions):
         pygame.event.pump()
@@ -72,7 +76,7 @@ class GameState:
             if self.playery > -2 * PLAYER_HEIGHT:
                 self.playerVelY = self.playerFlapAcc
                 self.playerFlapped = True
-                #SOUNDS['wing'].play()
+                # SOUNDS['wing'].play()
 
         # check for score
         playerMidPos = self.playerx + PLAYER_WIDTH / 2
@@ -80,7 +84,7 @@ class GameState:
             pipeMidPos = pipe['x'] + PIPE_WIDTH / 2
             if pipeMidPos <= playerMidPos < pipeMidPos + 4:
                 self.score += 1
-                #SOUNDS['point'].play()
+                # SOUNDS['point'].play()
                 reward = 1
 
         # playerIndex basex change
@@ -105,7 +109,7 @@ class GameState:
 
         # add new pipe when first pipe is about to touch left of screen
         if 0 < self.upperPipes[0]['x'] < 5:
-            newPipe = getRandomPipe()
+            newPipe = getRandomPipePair()
             self.upperPipes.append(newPipe[0])
             self.lowerPipes.append(newPipe[1])
 
@@ -115,18 +119,18 @@ class GameState:
             self.lowerPipes.pop(0)
 
         # check if crash here
-        isCrash= checkCrash({'x': self.playerx, 'y': self.playery,
-                             'index': self.playerIndex},
-                            self.upperPipes, self.lowerPipes)
+        isCrash = checkCrash({'x': self.playerx, 'y': self.playery,
+                              'index': self.playerIndex},
+                             self.upperPipes, self.lowerPipes)
         if isCrash:
-            #SOUNDS['hit'].play()
-            #SOUNDS['die'].play()
+            # SOUNDS['hit'].play()
+            # SOUNDS['die'].play()
             terminal = True
             self.__init__()
             reward = -1
 
         # draw sprites
-        SCREEN.blit(IMAGES['background'], (0,0))
+        SCREEN.blit(IMAGES['background'], (0, 0))
 
         for uPipe, lPipe in zip(self.upperPipes, self.lowerPipes):
             SCREEN.blit(IMAGES['pipe'][0], (uPipe['x'], uPipe['y']))
@@ -141,18 +145,20 @@ class GameState:
         image_data = pygame.surfarray.array3d(pygame.display.get_surface())
         pygame.display.update()
         FPSCLOCK.tick(FPS)
-        #print self.upperPipes[0]['y'] + PIPE_HEIGHT - int(BASEY * 0.2)
+        # print self.upperPipes[0]['y'] + PIPE_HEIGHT - int(BASEY * 0.2)
         return image_data, reward, terminal
 
-def getRandomPipe():
-    """returns a randomly generated pipe"""
-    # y of gap between upper and lower pipe
-    gapYs = [20, 30, 40, 50, 60, 70, 80, 90]
-    index = random.randint(0, len(gapYs)-1)
-    gapY = gapYs[index]
 
-    gapY += int(BASEY * 0.2)
-    pipeX = SCREENWIDTH + 10
+def getRandomPipePair():
+    """returns a randomly generated pipe"""
+    # y of the gap between upper and lower pipe
+    # gapYs = [20, 30, 40, 50, 60, 70, 80, 90]
+    # index = random.randint(0, len(gapYs) - 1)
+    # gapY = gapYs[index]
+    gapY = random.randint(20, 90)
+
+    gapY += int(BASEY * 0.2)  # new pipe y position, should be higher than the base
+    pipeX = SCREENWIDTH + 10  # new pipe x position, should be out of screen
 
     return [
         {'x': pipeX, 'y': gapY - PIPE_HEIGHT},  # upper pipe
@@ -163,7 +169,7 @@ def getRandomPipe():
 def showScore(score):
     """displays score in center of screen"""
     scoreDigits = [int(x) for x in list(str(score))]
-    totalWidth = 0 # total width of all numbers to be printed
+    totalWidth = 0  # total width of all numbers to be printed
 
     for digit in scoreDigits:
         totalWidth += IMAGES['numbers'][digit].get_width()
@@ -187,7 +193,7 @@ def checkCrash(player, upperPipes, lowerPipes):
     else:
 
         playerRect = pygame.Rect(player['x'], player['y'],
-                      player['w'], player['h'])
+                                 player['w'], player['h'])
 
         for uPipe, lPipe in zip(upperPipes, lowerPipes):
             # upper and lower pipe rects
@@ -208,6 +214,7 @@ def checkCrash(player, upperPipes, lowerPipes):
 
     return False
 
+
 def pixelCollision(rect1, rect2, hitmask1, hitmask2):
     """Checks if two objects collide and not just their rects"""
     rect = rect1.clip(rect2)
@@ -220,6 +227,6 @@ def pixelCollision(rect1, rect2, hitmask1, hitmask2):
 
     for x in range(rect.width):
         for y in range(rect.height):
-            if hitmask1[x1+x][y1+y] and hitmask2[x2+x][y2+y]:
+            if hitmask1[x1 + x][y1 + y] and hitmask2[x2 + x][y2 + y]:
                 return True
     return False
